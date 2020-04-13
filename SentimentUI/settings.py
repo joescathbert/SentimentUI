ca44679ac8fe11d6be9ad88787df359800decfd8
joescathbert <joescathbert@gmail.com>
2020-04-13 15:39:36 +0530

    Initial commit

--- SentimentUI/settings.py
+++ SentimentUI/settings.py
@@ -124,5 +124,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+PROJECT_ROOT   =   os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
